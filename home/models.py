eb20528cefdf02f5435d68d2ee95ebe39430cdca
AdarshVarma9395 <adarshvarma1999@gmail.com>
2023-12-26 14:50:01 +0530

    i've done some html changes

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class student(models.Model):
+    name = models.CharField(max_length = 100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField()
+    image = models.ImageField(upload_to='student_images/', default='default_image.jpg')
+    file = models.FileField()
+
+
+class product(models.Model):
+    pass
 # Create your models here.
